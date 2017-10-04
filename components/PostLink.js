bea973505a681fe799cdc2fd337f6f9ce7b1fec7
yann <yann.boisselier@gmail.com>
2017-10-04 16:45:47 +0200

    fix packages versions

--- components/PostLink.js
+++ components/PostLink.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const PostLink = ({post}) => (
   <li>
-    <Link as={`${post.url.alias}`} href={`/post?uuid=${post.uuid}`}>
+    <Link as={`${post.url.alias}`} prefetch href={`/post?uuid=${post.uuid}`}>
       <a>{post.title}</a>
     </Link>
   </li>
