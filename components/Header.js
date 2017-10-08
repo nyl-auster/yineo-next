02cb968b4f54a94f836f598a30d1a67e6c0ad8d2
yann <yann.boisselier@gmail.com>
2017-10-08 20:29:24 +0200

    css menu & header with bulma

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
 const Header = () => (
-  <div className="container">
-    <div className="title">YANN BOISSELIER</div>
-    <div className="subtitle">Développeur React, Next.js, Vue et Nuxt.js à Nantes </div>
-  </div>
+  <section className="hero">
+    <div className="hero-body">
+      <div className="container">
+        <div className="title">YANN BOISSELIER</div>
+        <div className="subtitle">Développeur React, Next.js, Vue et Nuxt.js à Nantes </div>
+      </div>
+    </div>
+  </section>
 )
 
 export default Header
--- components/Menu.js
+++ components/Menu.js
@@ -2,15 +2,19 @@ import { Link } from '../routes'
 
 const Menu = () => (
   <div className="container">
-    <Link prefetch route='HomePage'>
-      <a>Accueil </a>
-    </Link>
-    <Link prefetch route='PostListPage'>
-      <a>Blog </a>
-    </Link>
-    <Link prefetch route='ContactPage'>
-      <a>Contact </a>
-    </Link>
+    <div className="level">
+      <div className="level-left">
+        <Link prefetch route='HomePage'>
+          <a className="level-item">Accueil </a>
+        </Link>
+        <Link prefetch route='PostListPage'>
+          <a className="level-item">Blog </a>
+        </Link>
+        <Link prefetch route='ContactPage'>
+          <a className="level-item">Contact </a>
+        </Link>
+      </div>
+    </div>
   </div>
 )
 
