e33b970150c1f1bdb8ff8e97bfd8b8f601f436b2
yann <yann.boisselier@gmail.com>
2017-10-08 19:04:36 +0200

    add loading progress bar

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,12 @@ import Link from 'next/link'
 import Header from './Header'
 import Head from 'next/head'
 import Menu from './Menu'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+Router.onRouteChangeStart = (url) => NProgress.start()
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 const Layout = ({ children }) => (
   <div>
@@ -10,6 +16,7 @@ const Layout = ({ children }) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="stylesheet" type="text/css" href="/static/css/bulma-5.1.css" />
       <link rel="stylesheet" type="text/css" href="/static/css/app.css" />
+      <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css" />
     </Head>
     <Header />
     <Menu />
